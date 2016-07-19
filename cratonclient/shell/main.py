687bb6b34f0297055654bf03335bdab23a61cc92
Chris Spencer <christopher.m.spencer@intel.com>
2016-07-19 14:34:16 -0700

    Added optional arguments to host-list command and unit tests.
    Implemented handling of argument values as well as defined acceptable field values for hosts.
    
    Change-Id: I73d14529dc7baec3b98deb78313e924d5a314852

--- cratonclient/shell/main.py
+++ cratonclient/shell/main.py
@@ -19,6 +19,13 @@ import sys
 
 from oslo_utils import encodeutils
 
+from cratonclient import __version__
+from cratonclient import session as craton
+
+from cratonclient.common import cliutils
+from cratonclient.shell.v1 import shell
+from cratonclient.v1 import client
+
 
 class CratonShell(object):
     """Class used to handle shell definition and parsing."""
@@ -36,17 +43,94 @@ class CratonShell(object):
 
         parser.add_argument('-h', '--help',
                             action='store_true',
-                            help=argparse.SUPPRESS)
-
+                            help=argparse.SUPPRESS,
+                            )
+        parser.add_argument('--version',
+                            action='version',
+                            version=__version__,
+                            )
+        parser.add_argument('--craton-url',
+                            default=cliutils.env('CRATON_URL'),
+                            help='Defaults to env[CRATON_URL]',
+                            )
+        parser.add_argument('--craton-project-id',
+                            type=int,
+                            default=1,
+                            help='Defaults to 1',
+                            )
+        parser.add_argument('--os-username',
+                            default=cliutils.env('OS_USERNAME'),
+                            help='Defaults to env[OS_USERNAME]',
+                            )
+        parser.add_argument('--os-password',
+                            default=cliutils.env('OS_PASSWORD'),
+                            help='Defaults to env[OS_PASSWORD]',
+                            )
         return parser
+
+    # NOTE(cmspence): Credit for this get_subcommand_parser function
+    # goes to the magnumclient developers and contributors.
+    def get_subcommand_parser(self):
+        """Get subcommands by parsing COMMAND_MODULES."""
+        parser = self.get_base_parser()
+
+        self.subcommands = {}
+        subparsers = parser.add_subparsers(metavar='<subcommand>',
+                                           dest='subparser_name')
+        command_modules = shell.COMMAND_MODULES
+        for command_module in command_modules:
+            self._find_subparsers(subparsers, command_module)
+        self._find_subparsers(subparsers, self)
+        return parser
+
+    # NOTE(cmspence): Credit for this function goes to the
+    # magnumclient developers and contributors.
+    def _find_subparsers(self, subparsers, actions_module):
+        """Find subparsers by looking at *_shell files."""
+        help_formatter = argparse.HelpFormatter
+        for attr in (a for a in dir(actions_module) if a.startswith('do_')):
+            command = attr[3:].replace('_', '-')
+            callback = getattr(actions_module, attr)
+            desc = callback.__doc__ or ''
+            action_help = desc.strip()
+            arguments = getattr(callback, 'arguments', [])
+            subparser = (subparsers.add_parser(command,
+                                               help=action_help,
+                                               description=desc,
+                                               add_help=False,
+                                               formatter_class=help_formatter)
+                         )
+            subparser.add_argument('-h', '--help',
+                                   action='help',
+                                   help=argparse.SUPPRESS)
+            self.subcommands[command] = subparser
+            for (args, kwargs) in arguments:
+                subparser.add_argument(*args, **kwargs)
+            subparser.set_defaults(func=callback)
 
     def main(self, argv):
         """Main entry-point for cratonclient shell argument parsing."""
         parser = self.get_base_parser()
         (options, args) = parser.parse_known_args(argv)
+        subcommand_parser = (
+            self.get_subcommand_parser()
+        )
+        self.parser = subcommand_parser
+
         if options.help or not argv:
             parser.print_help()
             return 0
+
+        args = subcommand_parser.parse_args(argv)
+
+        session = craton.Session(
+            username=args.os_username,
+            token=args.os_password,
+            project_id=args.craton_project_id,
+        )
+        self.cc = client.Client(session, args.craton_url)
+
+        args.func(self.cc, args)
 
 
 def main():
---
+++ cratonclient/shell/v1/hosts_shell.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Hosts resource and resource shell wrapper."""
+from cratonclient.common import cliutils
+from cratonclient import exceptions as exc
+from cratonclient.v1.hosts import HOST_FIELDS as h_fields
+
+
+@cliutils.arg('-c', '--cell',
+              metavar='<cell>',
+              type=int,
+              help='Integer ID of the cell that contains '
+                   'the desired list of hosts.')
+@cliutils.arg('--detail',
+              action='store_true',
+              default=False,
+              help='Show detailed information about the hosts.')
+@cliutils.arg('--limit',
+              metavar='<limit>',
+              type=int,
+              help='Maximum number of hosts to return.')
+@cliutils.arg('--sort-key',
+              metavar='<field>',
+              help='Host field that will be used for sorting.')
+@cliutils.arg('--sort-dir',
+              metavar='<direction>',
+              default='asc',
+              help='Sort direction: "asc" (default) or "desc".')
+@cliutils.arg('--fields',
+              nargs='+',
+              metavar='<fields>',
+              default=[],
+              help='Comma-separated list of fields to display. '
+                   'Only these fields will be fetched from the server. '
+                   'Can not be used when "--detail" is specified')
+def do_host_list(cc, args):
+    """Print list of hosts which are registered with the Craton service."""
+    params = {}
+    default_fields = ['id', 'name', 'type', 'active', 'cell_id']
+    if args.cell is not None:
+        params['cell'] = args.cell
+    if args.limit is not None:
+        if args.limit < 0:
+            raise exc.CommandError('Invalid limit specified. Expected '
+                                   'non-negative limit, got {0}'
+                                   .format(args.limit))
+        params['limit'] = args.limit
+    if args.detail:
+        fields = h_fields
+        params['detail'] = args.detail
+    elif args.fields:
+        fields = {x: h_fields[x] for x in args.fields}
+    else:
+        fields = {x: h_fields[x] for x in default_fields}
+    if args.sort_key is not None:
+        fields_map = dict(zip(fields.keys(), fields.keys()))
+        # TODO(cmspence): Do we want to allow sorting by field heading value?
+        try:
+            sort_key = fields_map[args.sort_key]
+        except KeyError:
+            raise exc.CommandError(
+                '{0} is an invalid key for sorting,  valid values for '
+                '--sort-key are: {1}'.format(args.sort_key, h_fields.keys())
+            )
+        params['sort_key'] = sort_key
+        if args.sort_dir is not None:
+            if args.sort_dir not in ('asc', 'desc'):
+                raise exc.CommandError('Invalid sort direction specified. The '
+                                       'expected valid values for --sort-dir '
+                                       'are: "asc", "desc".')
+        params['sort_dir'] = args.sort_dir
+
+    hosts = cc.hosts.list(args.craton_project_id, **params)
+    cliutils.print_list(hosts, fields.keys())
