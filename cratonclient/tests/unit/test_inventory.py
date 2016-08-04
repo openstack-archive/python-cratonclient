1bfb4091db2ebc270a1de8aecf36c38c28974a4f
Chris Spencer <christopher.m.spencer@intel.com>
2016-08-04 08:51:02 -0700

    Adding Inventory class.
    Inventory will be access point for all sub entities to create an
    intuitive API for python developers to use when trying to access
    inventory entities (cells, hosts, etc).
    
    Change-Id: Icea217103c735ad53f029dac9cfd405281a1a715

---
+++ cratonclient/tests/unit/test_inventory.py
@@ -0,0 +1,31 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+"""Tests for `cratonclient.v1.inventory` module."""
+
+import mock
+
+from cratonclient.tests import base
+from cratonclient.v1 import inventory
+
+
+class TestHostsShell(base.TestCase):
+    """Test our craton inventory api class."""
+
+    @mock.patch('cratonclient.v1.hosts.HostManager')
+    def test_inventory_creates_host_manager(self, mock_hostmanager):
+        """Verify Inventory class creates HostManager."""
+        session = mock.Mock()
+        url = 'https://10.1.1.0:8080/'
+        region_id = 1,
+        inventory.Inventory(session, url, region_id)
+        mock_hostmanager.assert_called_once_with(session, url)
