cf4ab2dd8c7abf9855fbe182e672b70bb9b91bc0
Chris Spencer <christopher.m.spencer@intel.com>
2016-08-04 08:51:02 -0700

    Adding Inventory class.
    Inventory will be access point for all sub entities to create an
    intuitive API for python developers to use when trying to access
    inventory entities (cells, hosts, etc).
    
    Change-Id: Icea217103c735ad53f029dac9cfd405281a1a715

--- cratonclient/v1/inventory.py
+++ cratonclient/v1/inventory.py
@@ -12,17 +12,15 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 """Top-level client for version 1 of Craton's API."""
+
 from cratonclient.v1 import hosts
-from cratonclient.v1 import inventory
-from cratonclient.v1 import regions
 
 
-class Client(object):
-    """Craton v1 API Client."""
+class Inventory(object):
+    """Representation of the viewable inventory."""
 
-    def __init__(self, session, url):
+    def __init__(self, session, url, region_id):
         """Initialize our client object with our session and url.
-
         :param session:
             Initialized Session object.
         :type session:
@@ -31,21 +29,7 @@ class Client(object):
             The URL that points us to the craton instance. For example,
             'https://10.1.1.0:8080/'.
         """
-        self._url = url
-        self._session = session
 
-        if not self._url.endswith('/v1'):
-            self._url += '/v1'
-
-        manager_kwargs = {'session': self._session, 'url': url}
-        # TODO(cmspence):remove self.hosts
-        self.hosts = hosts.HostManager(**manager_kwargs)
-        self.regions = regions.RegionManager(**manager_kwargs)
-        # TODO(cmspence):self.projects, self.users
-
-    def inventory(self, region_id):
-        """Retrieve inventory for a given region."""
-        return inventory.Inventory(self._session,
-                                   self._url,
-                                   region_id)
-
+        # TODO(cmspence): self.region = self.regions.get(region=region_id)
+        self.hosts = hosts.HostManager(session, url)
+        # TODO(cmspence): self.users, self.projects, self.workflows
