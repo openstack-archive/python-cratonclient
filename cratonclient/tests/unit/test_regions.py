7eb544ac8ee7f10b23608a9503152a6d06f610ac
Chris Spencer <christopher.m.spencer@intel.com>
2016-08-18 15:07:09 -0700

    Adding support for creating a region.
    Adding ability to create region in both python and CLI clients.
    Adding doctests and unit tests for region creation.
    
    Change-Id: I59bcf4e727d3fd7ed6d55a58560fcf48eed8cb3d
    Implements: blueprint craton-client-access-inventory (partial)
    Closes-Bug: #1611418

---
+++ cratonclient/tests/unit/test_regions.py
@@ -0,0 +1,69 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests the region python client API in `cratonclient.v1.regions` module."""
+import json
+import requests_mock
+
+from cratonclient import exceptions as exc
+from cratonclient.tests import base
+
+
+class TestRegions(base.TestCase):
+    """Test craton regions python client."""
+
+    def new_region(self, **kwargs):
+        """Create a new Region test object."""
+        kwargs.setdefault('id', 1)
+        kwargs.setdefault('project_id', 1)
+        kwargs.setdefault('name', 'region')
+        return kwargs
+
+    def new_error(self, status, message):
+        """Create a mew error response object."""
+        return {'status': status, 'message': message}
+
+    @requests_mock.mock()
+    def test_region_create_success(self, m):
+        """Verify all required args results in success."""
+        from cratonclient import session
+        from cratonclient.v1 import client
+
+        reg = self.new_region()
+        m.post('http://example.com/regions',
+               text=json.dumps(reg),
+               status_code=200)
+        session = session.Session(
+            username='demo',
+            token='password',
+            project_id='1')
+        client = client.Client(session, 'http://example.com')
+        region = client.regions.create(name='region')
+        self.assertEqual(reg, region._info)
+
+    @requests_mock.mock()
+    def test_region_create_unknown_error(self, m):
+        """Verify what happens when an exception is raised."""
+        from cratonclient import session
+        from cratonclient.v1 import client
+
+        error = self.new_error(500, 'Unknown Error')
+        m.post('http://example.com/regions',
+               text=json.dumps(error),
+               status_code=500)
+        session = session.Session(
+            username='demo',
+            token='password',
+            project_id='1')
+        client = client.Client(session, 'http://example.com')
+        self.assertRaises(exc.InternalServerError,
+                          client.regions.create,
+                          name='test')
