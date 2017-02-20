223c89fbfbadca18a6b2c6515eddd03437bd1c0a
Jim Baker <jim.baker@python.org>
2017-02-20 10:53:16 -0700

    WIP: CLI and client support for get/set/delete of resource vars
    Functionality is supported both	for the	Python client:
    
    host = craton.hosts.get(item_id=8)
    host.variables.update(x='foo', y={'a': 47, 'b': True}, z='baz')
    
    As well	as for the CLI:
    
    craton region-vars-get 1
    craton cell-vars-get 3 x=true y=47 z=foo/bar w=3.14159
    cat <<EOF | craton host-vars-set 13
    {
        "glance_default_store": "not-so-swift",
        "neutron_l2_population": false,
        "make_stuff_up": true,
        "some/namespaced/variable": {"a": 1, "b": 2}
    }
    EOF
    craton host-vars-get --format=json 13 | jq -C
    craton cell-vars-delete 13 make_stuff_up
    craton cell-vars-set 13 x= y=42   # deletes x
    
    etc.
    
    The following resources are supported: regions, cells, hosts. Network
    devices will be supported before we complete this change (so still
    WIP) with the subcommand convention of <resources>-vars-get,
    <resources>-vars-set, and <resource>-vars-delete.
    
     All resources that currently support variables -- region, cell, host,
    rent resource getters/setters/deleters are <resource>region-get-vars,
    region-set-vars, cell-get-vars, cell-set-vars, host-get-vars,
    host-set-vars. Code is implemented to minimize the overhead of doing
    this for additional resources in the future, such as project, user,
    workflow, etc.
    
    It would be more ergonomic if the CLI supported referencing a
    resource, like a host, by an identifying name, not just an ID, but
    this work will be done in a separate branch.
    
    CURRENTLY NO UNIT TESTING for new work!!! This needs to be fixed
    before this work can be merged in.
    
    Change-Id: Id30188937518d7103d6f943cf1d038b039dc30cc

--- cratonclient/formatters/base.py
+++ cratonclient/formatters/base.py
@@ -11,6 +11,7 @@
 # under the License.
 """Base class implementation for formatting plugins."""
 from cratonclient import crud
+from cratonclient.v1 import variables
 
 
 class Formatter(object):
@@ -66,7 +67,8 @@ class Formatter(object):
             we will not know how to handle it. In that case, we will raise a
             ValueError.
         """
-        if isinstance(item_to_format, crud.Resource):
+        to_dict = getattr(item_to_format, 'to_dict', None)
+        if to_dict is not None:
             self.handle_instance(item_to_format)
             return
 
