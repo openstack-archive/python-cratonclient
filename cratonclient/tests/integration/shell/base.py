be56e0ff6182b7db98066786ba97442cbdcc0841
Ian Cordasco <graffatcolmingov@gmail.com>
2017-02-23 10:09:27 -0600

    Reorganize shell based integration tests
    This now follows the same organization as the unit tests have in
    cratonclient.
    
    Change-Id: If7a4397cec8010adc50a591ec536bf8142197442

--- cratonclient/tests/integration/shell/base.py
+++ cratonclient/tests/integration/shell/base.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2010-2011 OpenStack Foundation
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
-#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -14,7 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-"""Some integration testing utilities."""
+"""Resources for the shell integration tests."""
 
 import mock
 import six
--- cratonclient/tests/integration/shell/v1/test_main_shell.py
+++ cratonclient/tests/integration/shell/v1/test_main_shell.py
@@ -19,7 +19,7 @@ import re
 from testtools import matchers
 
 from cratonclient.shell import main
-from cratonclient.tests.integration import base
+from cratonclient.tests.integration.shell import base
 
 
 class TestMainShell(base.ShellTestCase):
