5f8d5a37e6180675830b3680fadeb4450e73a4c4
Ian Cordasco <graffatcolmingov@gmail.com>
2017-02-23 10:09:27 -0600

    Reorganize shell based integration tests
    This now follows the same organization as the unit tests have in
    cratonclient.
    
    Change-Id: If7a4397cec8010adc50a591ec536bf8142197442

---
+++ cratonclient/tests/integration/shell/base.py
@@ -0,0 +1,34 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Resources for the shell integration tests."""
+
+import mock
+import six
+
+from cratonclient.shell import main
+from cratonclient.tests import base
+
+
+class ShellTestCase(base.TestCase):
+    """Test case base class for all shell unit tests."""
+
+    def shell(self, arg_str, exitcodes=(0,)):
+        """Main function for exercising the craton shell."""
+        with mock.patch('sys.stdout', new=six.StringIO()) as mock_stdout, \
+            mock.patch('sys.stderr', new=six.StringIO()) as mock_stderr:
+
+            try:
+                main_shell = main.CratonShell()
+                main_shell.main(arg_str.split())
+            except SystemExit:
+                pass
+            return (mock_stdout.getvalue(), mock_stderr.getvalue())
--- cratonclient/tests/integration/shell/v1/__init__.py
+++ cratonclient/tests/integration/shell/v1/__init__.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2010-2011 OpenStack Foundation
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
-#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -14,4 +9,4 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-"""Integration test ."""
+"""Test suite for Craton client's v1 API shell."""
