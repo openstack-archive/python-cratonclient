e1f554595ed5958360fbb6e533da85de7660d658
Thomas Maddox <thomas.maddox@rackspace.com>
2017-03-06 16:37:52 +0000

    Adds remaining shell tests for variables
    This patch includes the shell integration tests
    for remaining Resources' variables implementation
    
    Change-Id: I44dc2c3463715321f70bb48514a70906d0c864ec
    Closes-Bug: 1659110

--- cratonclient/tests/integration/shell/v1/test_regions_shell.py
+++ cratonclient/tests/integration/shell/v1/test_regions_shell.py
@@ -19,6 +19,7 @@ from testtools import matchers
 from cratonclient.shell.v1 import regions_shell
 from cratonclient.tests.integration.shell import base
 from cratonclient.v1 import regions
+from cratonclient.v1 import variables
 
 
 class TestRegionsShell(base.ShellTestCase):
@@ -172,3 +173,79 @@ class TestRegionsShell(base.ShellTestCase):
             autopaginate=False,
         )
         mock_list.reset_mock()
+
+
+class TestRegionsVarsShell(base.VariablesTestCase):
+    """Test Region Variable shell calls."""
+
+    def setUp(self):
+        """Basic set up for all tests in this suite."""
+        super(TestRegionsVarsShell, self).setUp()
+
+        # NOTE(thomasem): Mock out a client to assert craton Python API calls
+        self.client = mock.Mock()
+        self.mock_region_resource = self.client.regions.get.return_value
+        self.mock_region_resource.variables = variables.VariableManager(
+            self.mock_session, self.resource_url
+        )
+
+    def test_do_region_vars_get_gets_correct_region(self):
+        """Assert the proper region is retrieved when calling get."""
+        self.mock_get_response.json.return_value = \
+            {"variables": {"foo": "bar"}}
+        regions_shell.do_region_vars_get(self.client, self.test_args)
+        self.client.regions.get.assert_called_once_with(
+            vars(self.test_args)['id'])
+
+    def test_do_region_vars_delete_gets_correct_region(self):
+        """Assert the proper region is retrieved when calling delete."""
+        self.test_args.variables = ['foo', 'bar']
+        regions_shell.do_region_vars_delete(self.client, self.test_args)
+        self.client.regions.get.assert_called_once_with(
+            vars(self.test_args)['id'])
+
+    def test_do_region_vars_update_gets_correct_region(self):
+        """Assert the proper region is retrieved when calling update."""
+        self.test_args.variables = ['foo=', 'bar=']
+        mock_resp_json = {"variables": {"foo": "bar"}}
+        self.mock_get_response.json.return_value = mock_resp_json
+        self.mock_put_response.json.return_value = mock_resp_json
+
+        regions_shell.do_region_vars_set(self.client, self.test_args)
+        self.client.regions.get.assert_called_once_with(
+            vars(self.test_args)['id'])
+
+    def test_do_region_vars_get_calls_session_get(self):
+        """Assert the proper region is retrieved when calling get."""
+        self.mock_get_response.json.return_value = \
+            {"variables": {"foo": "bar"}}
+        regions_shell.do_region_vars_get(self.client, self.test_args)
+        self.mock_session.get.assert_called_once_with(self.variables_url)
+
+    def test_do_region_vars_delete_calls_session_delete(self):
+        """Verify that do region-vars-delete calls expected session.delete."""
+        self.test_args.variables = ['foo', 'bar']
+        regions_shell.do_region_vars_delete(self.client, self.test_args)
+        self.mock_session.delete.assert_called_once_with(
+            self.variables_url,
+            json=('foo', 'bar'),
+            params={},
+        )
+
+    def test_do_region_vars_update_calls_session_put(self):
+        """Verify that do region-vars-delete calls expected session.delete."""
+        self.test_args.variables = ['foo=baz', 'bar=boo', 'test=']
+        mock_resp_json = {"variables": {"foo": "bar"}}
+        self.mock_get_response.json.return_value = mock_resp_json
+        self.mock_put_response.json.return_value = mock_resp_json
+
+        regions_shell.do_region_vars_set(self.client, self.test_args)
+        self.mock_session.delete.assert_called_once_with(
+            self.variables_url,
+            json=('test',),
+            params={},
+        )
+        self.mock_session.put.assert_called_once_with(
+            self.variables_url,
+            json={'foo': 'baz', 'bar': 'boo'}
+        )
